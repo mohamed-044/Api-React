b1f3a9e44f21ceba56afe79b586f8a48d79393da
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-06-23 11:32:16 +0200

    get product from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-  return (
-<>
-</>
-  )
+
+function Product() {
+  const [product, setProduct] = useState(null);
+  
+  useEffect(() => {
+    async function fetchProduct() {
+      const response = await fetch("https://fakestoreapi.com/products/1")
+      const data = await response.json();
+      setProduct(data);
+      
+    }
+
+
+  fetchProduct();
+}, []);
+
+
+
+  return ( product &&
+<div>
+  <h2>{product.title}</h2>
+  <img src={product.image} alt={product.title} />
+  <p><strong>Prix :</strong>{product.price} €</p>
+  <p>{product.description}</p>
+</div>
+  );
 }
 
-export default App
+export default Product;
