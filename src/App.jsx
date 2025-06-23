fb82cd87691e445c02da9e8761e6249b18786fff
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-06-23 15:48:19 +0200

    add product with button and alert

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
+import { Button} from 'react-bootstrap';
+
 
 function Product() {
   const [product, setProduct] = useState([]);
+  const [newProductId, setNewProductId] = useState(null);
 
   useEffect(() => {
     async function fetchProduct() {
@@ -17,12 +20,37 @@ function Product() {
     fetchProduct();
   }, []);
 
+
+  
+
+    async function addProduct() {
+      const response = await fetch("https://fakestoreapi.com/products",{
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: "Nouveau produit",
+          price: 29.99,
+          description: "Un produit tout nouveau",
+          image : "https://via.placeholder/150",
+          category: "electronics",
+        }),
+    });
+    const data = await response.json();
+    setNewProductId(data.id);
+    alert(`Le produit avec l'id ${data.id} a été créé`);
+  }
+
+
   if (product.length === 0) {
     return <p>Chargement...</p>;
   }
   
   return (
-  <Container className="py-4">
+  
+  <Container className="py-4" >
+    <Button variant="primary" onClick={addProduct}>Ajouter un produit</Button>
     <Row xs={1} md={4} className="g-3">
         {product.map(product => (
             <Col key={product.id} >
