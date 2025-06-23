b2d8c46bfd299579e201bc9d87a297527fa9bc0a
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-06-23 12:08:27 +0200

    show all products with title, description and price

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-
+import Card from 'react-bootstrap/Card';
 
 function Product() {
-  const [product, setProduct] = useState(null);
-  
+  const [product, setProduct] = useState([]);
+
   useEffect(() => {
     async function fetchProduct() {
-      const response = await fetch("https://fakestoreapi.com/products/1")
+      const response = await fetch("https://fakestoreapi.com/products");
       const data = await response.json();
       setProduct(data);
-      
     }
 
+    fetchProduct();
+  }, []);
 
-  fetchProduct();
-}, []);
-
-
-
-  return ( product &&
-<div>
-  <h2>{product.title}</h2>
-  <img src={product.image} alt={product.title} />
-  <p><strong>Prix :</strong>{product.price} €</p>
-  <p>{product.description}</p>
-</div>
+  if (product.lenght === 0) {
+    return <p>Chargement...</p>;
+  }
+  console.log(product);
+  return (
+  <div className="product-container">
+      {product.map(product => (
+        <Card key={product.id} className="product-card">
+          <Card.Img variant="top" src={product.image} alt={product.title} className="product-image"/>
+          <Card.Body>
+            <Card.Title>{product.title}</Card.Title>
+            <Card.Text className="description"> {product.description}</Card.Text>
+            <p><strong>Prix :</strong> {product.price} €</p>
+          </Card.Body>
+        </Card>
+      ))}
+    </div>
   );
 }
 
