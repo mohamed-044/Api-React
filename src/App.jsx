e3596b044b8058b1062d31bb48485e9f187c1c35
mohamed-044 <mohamed.boulatika04@gmail.com>
2025-06-24 11:34:02 +0200

    formatting with prettier

--- src/App.jsx
+++ src/App.jsx
@@ -14,124 +14,134 @@ function Product() {
   useEffect(() => {
     async function fetchProduct() {
       try {
-      const response = await fetch("https://fakestoreapi.com/products");
-      if (!response.ok) {
-      throw new Error(`Erreur HTTP: ${response.statusText ? response.statusText + ' - ' :
-      ''}${response.status}`);
+        const response = await fetch("https://fakestoreapi.com/products");
+        if (!response.ok) {
+          throw new Error(
+            `Erreur HTTP: ${
+              response.statusText ? response.statusText + " - " : ""
+            }${response.status}`
+          );
+        }
+        const data = await response.json();
+        setProduct(data);
+        console.log("Produits récupérés :", data);
+      } catch (error) {
+        console.error(error.message);
+        alert("Une erreur est survenue lors de la récupération des produits.");
+      } finally {
+        setLoading(false);
       }
-      const data = await response.json();
-      setProduct(data);
-      console.log("Produits récupérés :", data);
-    } catch (error) {
-      console.error(error.message); 
-      alert("Une erreur est survenue lors de la récupération des produits.");
-    }
-    finally {
-    setLoading(false);
-    }
     }
     fetchProduct();
   }, []);
 
   async function addProduct() {
     try {
-    const response = await fetch("https://fakestoreapi.com/products", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: "Nouveau produit",
-        price: 29.99,
-        description: "Un produit tout nouveau",
-        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        category: "electronics",
-      }),
-    });
-    if (!response.ok) {
-    throw new Error(`Erreur HTTP: ${response.statusText ? response.statusText + ' - ' :
-    ''}${response.status}`);
+      const response = await fetch("https://fakestoreapi.com/products", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: "Nouveau produit",
+          price: 29.99,
+          description: "Un produit tout nouveau",
+          image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+          category: "electronics",
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(
+          `Erreur HTTP: ${
+            response.statusText ? response.statusText + " - " : ""
+          }${response.status}`
+        );
+      }
+      const data = await response.json();
+      console.log("Produit ajouté :", data);
+      alert(`Le produit avec l'id ${data.id} a été créé`);
+    } catch (error) {
+      console.error(error.message);
+      alert("Une erreur est survenue lors de l'ajout du produit.");
     }
-    const data = await response.json();
-    console.log("Produit ajouté :", data);
-    alert(`Le produit avec l'id ${data.id} a été créé`);
-  }
-  catch (error) {
-  console.error(error.message); 
-  alert("Une erreur est survenue lors de l'ajout du produit."); 
-  }
   }
 
   async function changeProduct(id) {
     try {
-    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title: "Produit modifié",
-        price: 49.99,
-        description: "Déscription modifiée",
-        image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-        category: "electronics",
-      }),
-    });
-    if (!response.ok) {
-    throw new Error(`Erreur HTTP: ${response.statusText ? response.statusText + ' - ' :
-    ''}${response.status}`);
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title: "Produit modifié",
+          price: 49.99,
+          description: "Déscription modifiée",
+          image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+          category: "electronics",
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(
+          `Erreur HTTP: ${
+            response.statusText ? response.statusText + " - " : ""
+          }${response.status}`
+        );
+      }
+      const data = await response.json();
+      console.log("Produit modifié :", data);
+      alert(`Le produit avec l'id ${data.id} a été modifié`);
+    } catch (error) {
+      console.error(error.message);
+      alert("Une erreur est survenue lors de la modification du produit."); // Pour le client
     }
-    const data = await response.json();
-    console.log("Produit modifié :", data);
-    alert(`Le produit avec l'id ${data.id} a été modifié`);
-  }
-  catch (error) {
-  console.error(error.message);
-  alert("Une erreur est survenue lors de la modification du produit."); // Pour le client
-  }
   }
 
   async function changePrice(id) {
     try {
-    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        price: 39.99,
-      }),
-    });
-    if (!response.ok) {
-    throw new Error(`Erreur HTTP: ${response.statusText ? response.statusText + ' - ' :
-    ''}${response.status}`);
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          price: 39.99,
+        }),
+      });
+      if (!response.ok) {
+        throw new Error(
+          `Erreur HTTP: ${
+            response.statusText ? response.statusText + " - " : ""
+          }${response.status}`
+        );
+      }
+      const data = await response.json();
+      console.log("Prix modifié :", data);
+      alert(`Le prix du produit avec l'id ${data.id} a été modifié`);
+    } catch (error) {
+      console.error(error.message);
+      alert("Une erreur est survenue lors de la récupération du produit.");
     }
-    const data = await response.json();
-    console.log("Prix modifié :", data);
-    alert(`Le prix du produit avec l'id ${data.id} a été modifié`);
-  }
-  catch (error) {
-  console.error(error.message); 
-  alert("Une erreur est survenue lors de la récupération du produit.");
-  }
   }
 
   async function deleteProduct(id) {
     try {
-    const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
-      method: "DELETE",
-    });
-    if (!response.ok) {
-    throw new Error(`Erreur HTTP: ${response.statusText ? response.statusText + ' - ' :
-    ''}${response.status}`);
-    }
-    const data = await response.json();
-    console.log("Produit supprimé :", data);
-    alert(`Le prix du produit avec l'id ${data.id} a été supprimé`);
-    }
-    catch (error) {
-    console.error(error.message);
-    alert("Une erreur est survenue lors de la récupération du produit.");
+      const response = await fetch(`https://fakestoreapi.com/products/${id}`, {
+        method: "DELETE",
+      });
+      if (!response.ok) {
+        throw new Error(
+          `Erreur HTTP: ${
+            response.statusText ? response.statusText + " - " : ""
+          }${response.status}`
+        );
+      }
+      const data = await response.json();
+      console.log("Produit supprimé :", data);
+      alert(`Le prix du produit avec l'id ${data.id} a été supprimé`);
+    } catch (error) {
+      console.error(error.message);
+      alert("Une erreur est survenue lors de la récupération du produit.");
     }
   }
 
